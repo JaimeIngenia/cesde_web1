61ed84e92d50843989a271c42b060339b6a2cc7e
jaime1315321 <jamoncayop@unal.edu.co>
2023-08-07 11:50:46 -0500

    juego parte 1 jaime

---
+++ 1aTrabajoColaborativo/app.js
@@ -0,0 +1,12 @@
+
+
+const secretWord = "javascript";
+let displayedWord = "_ ".repeat(secretWord.length);
+
+const wordDisplay = document.getElementById("wordDisplay");
+
+// Inicializar la pantalla de la palabra
+wordDisplay.textContent = displayedWord;
+
+
+       
